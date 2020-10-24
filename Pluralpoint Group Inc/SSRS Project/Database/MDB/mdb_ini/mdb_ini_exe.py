2d66be00250dfec6ca47fb77b66ae6fdf71a8544
Zacks <Zacks.Shen@gmail.com>
2020-10-23 20:47:12 -0700

    Create mdb_ini_exe.py for executing mdb_ini_Generator.py

---
+++ Pluralpoint Group Inc/SSRS Project/Database/MDB/mdb_ini/mdb_ini_exe.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+################################################################################
+# File Name: mdb_ini_exe.py                                                    #
+# File Path: /mdb_ini_exe.py                                                   #
+# Created Date: 2020-10-23                                                     #
+# -----                                                                        #
+# Company: Pluralpoint Group Inc.                                              #
+# Author: Zacks Shen                                                           #
+# Blog: https://zacks.one                                                      #
+# Email: <zacks.shen@pluralpoint.com>                                          #
+# Github: https://github.com/ZacksAmber                                        #
+# -----                                                                        #
+# Last Modified: 2020-10-23 8:43:01 pm                                         #
+# Modified By: Zacks Shen <zacks.shen@pluralpoint.com>                         #
+# -----                                                                        #
+# Copyright (c) 2020 Pluralpoint Group Inc.                                    #
+################################################################################
+
+from mdb_ini_Generator import mdb_ini_Generator
+
+obj = mdb_ini_Generator()            
+obj.getUserInput()
