1dcc7373200a69eae2c1b5b6e1c27f5a31bfe385
Zacks <Zacks.Shen@gmail.com>
2020-10-25 20:35:07 -0700

    update: add function calculate time consume

--- Pluralpoint Group Inc/SSRS Project/Database/MDB/mdb_ini/programs/mdb_ini_Exporter.py
+++ Pluralpoint Group Inc/SSRS Project/Database/MDB/mdb_ini/programs/mdb_ini_Exporter.py
@@ -11,7 +11,7 @@
 # Email: <zacks.shen@pluralpoint.com>                                          #
 # Github: https://github.com/ZacksAmber                                        #
 # -----                                                                        #
-# Last Modified: 2020-10-24 8:26:08 pm                                         #
+# Last Modified: 2020-10-25 7:43:15 pm                                         #
 # Modified By: Zacks Shen <zacks.shen@pluralpoint.com>                         #
 # -----                                                                        #
 # Copyright (c) 2020 Pluralpoint Group Inc.                                    #
@@ -24,13 +24,15 @@ This program is designed for passing parameters to the covertor then runing it.
 import os
 import sys
 import subprocess
+import datetime
 
 class mdb_ini_Exporter:
     def __init__(self):
+        self.programDir = os.getcwd()
         os.chdir("..")
         self.rootDir = os.getcwd()
 
-        wdCheck = input("Please make a directory to store the programs, and make another directory with the name 'mdb' to store the mdb files.\n1. Input 'y' to start.\n2 .Input anything to quit.\nYour input: ")
+        wdCheck = input("Please make a directory to store the programs, and make another directory with the name 'mdb' to store the mdb files.\n1. Input 'y' to start.\n2. Input anything to quit.\nYour input: ")
         
         if wdCheck not in ["y", "Y"]:
             sys.exit()
@@ -54,29 +56,25 @@ class mdb_ini_Exporter:
             sys.exit() 
 
     def getMySQL(self):
-        self.exePath = 'C:\\Program Files (x86)\\Bullzip\\MS Access to MySQL\\msa2mys.exe'
-        self.mysql_iniDir = self.rootDir + "\\mysql_ini\\"
-        os.chdir(self.mysql_iniDir)
+        exePath = 'C:\\Program Files (x86)\\Bullzip\\MS Access to MySQL\\msa2mys.exe'
+        iniDir = self.rootDir + "\\mysql_ini\\"
 
-        self.exportDB("MySQL")
-
+        self.exportDB(exePath, iniDir, "MySQL")
 
     def getMSSQL(self):
-        self.exePath = 'C:\\Program Files (x86)\\Bullzip\\MS Access to MSSQL\\msa2sql.exe'
-        self.mssql_iniDir = self.rootDir + "\\mssql_ini\\"
-        os.chdir(self.mssql_iniDir)
+        exePath = 'C:\\Program Files (x86)\\Bullzip\\MS Access to MSSQL\\msa2sql.exe'
+        mssql_iniDir = self.rootDir + "\\mssql_ini\\"
 
-        self.exportDB("MSSSQL")
+        self.exportDB(exePath, iniDir, "MSSSQL")
 
     def getPostgreSQL(self):
-        self.exePath = 'C:\\Program Files (x86)\\Bullzip\\MS Access to PostgreSQL\\msa2pgs.exe'
-        self.postgresql_iniDir = self.rootDir + "\\postgresql_ini\\"
-        os.chdir(self.postgresql_iniDir)
+        exePath = 'C:\\Program Files (x86)\\Bullzip\\MS Access to PostgreSQL\\msa2pgs.exe'
+        postgresql_iniDir = self.rootDir + "\\postgresql_ini\\"
 
-        self.exportDB("PostgreSQL")
+        self.exportDB(exePath, iniDir, "PostgreSQL")
 
-    def exportDB(self, databaseType):
-        os.
+    def exportDB(self, exePath, iniDir, databaseType):
+        os.chdir(iniDir)
         iniFiles = os.listdir()
         for iniFile in iniFiles:
             mdbName = iniFile # get the ini file name without extension and DB type
@@ -84,23 +82,76 @@ class mdb_ini_Exporter:
             mdbName = mdbName.split("_", 1)
             mdbName = mdbName[-1]
             mdbName = mdbName[::-1]
-            SETTINGS = "SETTINGS=" + self.iniFile
+            SETTINGS = "SETTINGS=" + iniFile
             args = [exePath, SETTINGS, ",AUTORUN", ",HIDE"] # define the parameters for the .exe
             proc = subprocess.Popen(args)
             try:
-                proc.wait() # wait until the current process finished
-                print("Process Running Succeed: " + iniFile)
-                outputLog(mdbName, "succeed!")
+                print("Exporting DB: " + mdbName)
+                startTime = datetime.datetime.now().time()
+                self.outputLog(mdbName=mdbName, databaseType=databaseType, status="succeed", startTime=startTime)
+                proc.wait()
+                #proc.wait() # wait until the current process finished
+                print("Export Succeed: " + mdbName)
+                endTime = datetime.datetime.now().time()
+                self.outputLog(mdbName=mdbName, databaseType=databaseType, status="succeed",  startTime=startTime, endTime=endTime)
             except:
                 proc.kill()
-                print("Process Running Failed: " + iniFile)
-                outputLog(mdbName, "failed!")
+                startTime = datetime.datetime.now().time()
+                print("Export Failed: " + mdbName)
+                self.outputLog(mdbName=mdbName, databaseType=databaseType, status="failed", startTime=startTime)
 
-    def outputLog(self, mdbName, databaseType, status):
+    def outputLog(self, mdbName=None, databaseType=None, status=None, startTime=None, endTime=None):
         os.chdir(self.programDir)
-        with open("mdb_ini_Exporter_log.txt", "a", newline="\r\n") as f:
-            f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ": Export DB " + mdbName + " to RDS " + databaseType + " " + status + "\n")
 
+        if status == "succeed":
+            if endTime == None:
+                with open("mdb_ini_Exporter_log.txt", "a", newline="\r\n") as f:
+                    f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " -- Exporting DB " + mdbName + " to RDS " + databaseType + ", mission start!\n")               
+            else:
+                procTime = datetime.timedelta(hours=endTime.hour, minutes=endTime.minute, seconds=endTime.second) - datetime.timedelta(hours=startTime.hour, minutes=startTime.minute, seconds=startTime.second)
+                with open("mdb_ini_Exporter_log.txt", "a", newline="\r\n") as f:
+                    f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " -- Exporting DB " + mdbName + " to RDS " + databaseType + ", mission " + status + "!\n")
+                    f.write("Time consume: " + str(procTime))
+                    f.write("\n")
+        else:
+            with open("mdb_ini_Exporter_log.txt", "a", newline="\r\n") as f:
+                f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " -- Exporting DB " + mdbName + " to RDS " + databaseType + ", mission " + status + "!\n")
+        
+        f.close()
+            
 # Execute the program
 obj = mdb_ini_Exporter()
 obj.main()
+
+'''
+programDir = os.getcwd()
+mdbName = "xtreme"
+databaseType = "MySQL"
+startTime = datetime.datetime.now().time()
+endTime = datetime.datetime.now().time()
+databaseType = "MySQL"
+
+outputLog(mdbName=mdbName, databaseType = databaseType, status="succeed", startTime=startTime)
+outputLog(mdbName=mdbName, databaseType = databaseType, status="succeed", startTime=startTime, endTime=endTime)
+outputLog(mdbName=mdbName, databaseType = databaseType, status="failed", startTime=startTime)
+
+
+def outputLog(mdbName=None, databaseType=None, status=None, startTime=None, endTime=None):
+    os.chdir(programDir)
+
+    if status == "succeed":
+        if endTime == None:
+            with open("mdb_ini_Exporter_log.txt", "a", newline="\r\n") as f:
+                f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " -- Exporting DB " + mdbName + " to RDS " + databaseType + ", mission start!\n")               
+        else:
+            procTime = datetime.timedelta(hours=endTime.hour, minutes=endTime.minute, seconds=endTime.second) - datetime.timedelta(hours=startTime.hour, minutes=startTime.minute, seconds=startTime.second)
+            with open("mdb_ini_Exporter_log.txt", "a", newline="\r\n") as f:
+                f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " -- Exporting DB " + mdbName + " to RDS " + databaseType + ", mission " + status + "!\n")
+                f.write("Time consume: " + str(procTime))
+                f.write("\n")
+    else:
+        with open("mdb_ini_Exporter_log.txt", "a", newline="\r\n") as f:
+            f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " -- Exporting DB " + mdbName + " to RDS " + databaseType + ", mission " + status + "!\n")
+    
+    f.close()
+'''
